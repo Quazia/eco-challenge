82db221244bb0a39e0619145043f45c3467ad592
Quazia <alf40k@gmail.com>
2023-07-14 11:26:07 -0400

    🏷️ typehash constant for secret

--- src/types/structs/Secret.sol
+++ src/types/structs/Secret.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.19;
 
+bytes32 constant SECRET_TYPEHASH = keccak256(
+    "Secret(address signerOne,address signerTwo,bytes secret)"
+);
+
 struct Secret {
     address signerOne;
     address signerTwo;
