9093e8e54e872c3594afdd4ad7440ddccaaaef46
Quazia <alf40k@gmail.com>
2023-07-14 11:26:10 -0400

    👔Reveal testing setup

--- test/SecretCommit.t.sol
+++ test/SecretCommit.t.sol
@@ -37,20 +37,32 @@ contract Commit is Test {
         assert(false);
     }
 
-    function test_RevertIf_CommitExists() {
+    function test_RevertIf_CommitExists() public {
         assert(false);
     }
 
-    function test_RevertIf_InvalidSender() {
+    function test_RevertIf_InvalidSender() public {
         assert(false);
     }
 }
 
 contract Reveal is Test {
     SecretCommit secretCommit;
+    address alice;
+    address bob;
+    uint256 akey;
+    uint256 bkey;
 
     function setUp() public {
+        (alice, akey) = makeAddrAndKey("alice");
+        (bob, bkey) = makeAddrAndKey("bob");
         secretCommit = new SecretCommit();
-        // Perform Commit Logic in setup
+        bytes memory secret = abi.encode("secret");
+        Secret memory secretStruct = Secret(alice, bob, secret);
+        bytes32 hashSecret = secretCommit.hashTypedData(secretStruct);
+        (uint8 v1, bytes32 r1, bytes32 s1) = vm.sign(akey, hashSecret);
+        (uint8 v2, bytes32 r2, bytes32 s2) = vm.sign(bkey, hashSecret);
+        vm.prank(alice);
+        secretCommit.commit(hashSecret, v1, r1, s1, v2, r2, s2);
     }
 }
