483345620292b8abe9a57da775d3ca840525e98b
Quazia <alf40k@gmail.com>
2023-07-14 11:26:13 -0400

    💡 SecretCommit initial comments

--- src/SecretCommit.sol
+++ src/SecretCommit.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.19;
+
 import {Secret, SECRET_TYPEHASH} from "src/types/structs/Secret.sol";
 import {Commitment, Signature} from "src/types/structs/Commitment.sol";
 import {EIP712} from "solady/utils/EIP712.sol";
 
+/// @notice This contract allows exactly two parties to commit to and reveal a secret
+/// @author Arthur Lunn
 contract SecretCommit is EIP712 {
     mapping(bytes32 => Commitment) public commitments;
     event Reveal(bytes indexed secret, address indexed revealer);
 
+    /// @dev Overriden to enable EIP712
+    /// @return name Name of this contract for use in domain separator
+    /// @return version Version of this contract for use in domain separator
     function _domainNameAndVersion()
         internal
         pure
@@ -18,6 +24,9 @@ contract SecretCommit is EIP712 {
         version = "1";
     }
 
+    /// @dev Gives us the hash of our main secret for use in EIP712 signing
+    /// @param secret Secret struct encapsulating the secret we want to sign
+    /// @return structHash Hash of the secret struct
     function hashStruct(
         Secret calldata secret
     ) internal pure returns (bytes32 structHash) {
@@ -32,16 +41,32 @@ contract SecretCommit is EIP712 {
             );
     }
 
+    /// @dev Leverages solady EIP712 to get full hash of our secret, domain separator and EIP-191 version byte
+    /// @param secret Secret struct encapsulating the secret we want to sign
+    /// @return digest Full hash of our secret, domain separator and EIP-191 version byte
     function hashTypedData(
         Secret calldata secret
     ) public view virtual returns (bytes32 digest) {
         return _hashTypedData(hashStruct(secret));
     }
 
-    function commitExists(bytes32 hashSecret) public view returns (bool) {
+    /// @notice Checks if a commit exists
+    /// @param hashSecret Hash of the secret we want to check
+    /// @return exists Boolean indicating if the commit exists
+    function commitExists(
+        bytes32 hashSecret
+    ) public view returns (bool exists) {
         return commitments[hashSecret].hashSecret == hashSecret;
     }
 
+    /// @notice Commits two parties to a secret by storing their signatures in a single block
+    /// @param hashSecret Hash of the secret we want to commit to
+    /// @param v1 Final byte of the signature of the first signer
+    /// @param r1 First 32 bytes of the signature of the first signer
+    /// @param s1 Second 32 bytes of the signature of the first signer
+    /// @param v2 Final byte of the signature of the second signer
+    /// @param r2 First 32 bytes of the signature of the second signer
+    /// @param s2 Second 32 bytes of the signature of the second signer
     function commit(
         bytes32 hashSecret,
         uint8 v1,
@@ -64,6 +89,8 @@ contract SecretCommit is EIP712 {
         );
     }
 
+    /// @notice Reveals a secret if the commit exists and the signatures are valid
+    /// @param secret Secret struct encapsulating the secret we want to reveal
     function reveal(Secret calldata secret) external {
         bytes32 digest = hashTypedData(secret);
         require(commitExists(digest), "Commit does not exist");
