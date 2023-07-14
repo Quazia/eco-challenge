de348cfd64a61c6b22bf1f52dfa46301ad0bdd77
Quazia <alf40k@gmail.com>
2023-07-14 11:26:07 -0400

    ✨ SecretCommit extends EIP712

--- src/SecretCommit.sol
+++ src/SecretCommit.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.19;
+import "solady/utils/EIP712.sol";
 
-contract SecretCommit {}
+contract SecretCommit is EIP712 {
+    function _domainNameAndVersion()
+        internal
+        pure
+        override
+        returns (string memory name, string memory version)
+    {
+        name = "SecretCommit";
+        version = "1";
+    }
+}
